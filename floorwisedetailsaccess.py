5116337bd95ad964f3bbf6b69efb8d01b6b44776
varunjoshi <133052176+varunjoshi@users.noreply.github.com>
2025-12-28 18:03:32 +0530

    Update floorwisedetailsaccess.py
    #if asked number of members per floor?
    #if asked names of members per floor

--- floorwisedetailsaccess.py
+++ floorwisedetailsaccess.py
@@ -36,7 +36,41 @@ elif floordetails == 2:
 elif floordetails == 3:
     print("Floor 3 members:", floor3)
 elif floordetails != 1 or 2 or 3 :
-     print("Please choose only from 1,2 or 3")
+     print("Please choose only from 1,2 or 3
+
+
+
+#if asked number of members per floor
+membersnum = input("Do you want to know number of members per floor? (Yes/No): ")
+if membersnum == "Yes" or "yes":
+    floormembers = int(input("Enter floor number (1/2/3): "))
+    if floormembers == 1:
+        print(f"Floor 1 has {len(floor1)} members")
+    elif floormembers == 2:
+        print(f"Floor 2 has {len(floor2)} members")
+    elif floormembers == 3:
+        print(f"Floor 3 has {len(floor3)} members")
+    else:
+        print("Please choose only from 1, 2 or 3")
+
+        #names per floor
+
+#if asked names per floor
+membersname = input("Do you want to know number of members per floor? (Yes/No): ")
+if membersname == "Yes" or "yes":
+
+    floormembers = int(input("Enter floor number (1/2/3): "))
+    if floormembers == 1:
+        print("Floor 1 names:", floor1[::2])  # Every 2nd = names only
+    elif floormembers == 2:
+        print("Floor 2 names:", floor2[::2])
+    elif floormembers == 3:
+        print("Floor 3 names:", floor3[::2])
+    else:
+        print("Please choose only from 1, 2 or 3")
+        
+else: 
+      print("Ok :)\t")
 
 
 
